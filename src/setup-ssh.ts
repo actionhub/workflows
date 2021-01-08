474ea67f290222e88778fd31f6c480362e054cd3
okou19900722 <ilayan19900722@gmail.com>
2021-01-08 20:10:28 +0800

    test

--- src/setup-ssh.ts
+++ src/setup-ssh.ts
@@ -1,41 +1,56 @@
 import os from "os";
 import path from "path";
 import fs from "fs";
-import execa from "execa";
+// import execa from "execa";
+import * as exec from "@actions/exec"
 import * as core from "@actions/core";
+import * as io from "@actions/io";
 import tmpDir from "./tmp-helper";
+import {v4 as uuid} from 'uuid';
+import execa from "execa";
 
 const HOME_DIR = process.env['HOME'] || os.homedir();
 const SSH_DIR = path.join(HOME_DIR, ".ssh");
 
+async function ensureDirectoryExists(path: string) {
+    if (fs.existsSync(path)) {
+        await io.mkdirP(path);
+        core.info(`create directory ${path}`)
+    }
+}
 export default async function setupSSH(privateKey: string, host: string, port: number) {
     try {
-        if (!port) {
-            port = 22
-        }
-        core.startGroup("Setup SSH")
-        // Create the required directory
-        fs.mkdirSync(SSH_DIR, {recursive: true})
-
-        core.info('Starting ssh-agent')
-
+        core.startGroup("Setup SSH");
+        await ensureDirectoryExists(SSH_DIR);
+        await ensureDirectoryExists(tmpDir);
+        const sshAgentPath = await io.which("ssh-agent");
+        const sshAddPath = await io.which("ssh-add");
+        const sshKeyScanPath = await io.which("ssh-keyscan");
+        core.info('start ssh-agent')
         // Start the ssh agent
-        const authSock = '/tmp/ssh-auth.sock'
-        await execa('ssh-agent', ['-a', authSock])
-        core.exportVariable('SSH_AUTH_SOCK', authSock)
+        const authSock = path.join(tmpDir, uuid() + ".sock");
+        await exec.exec(sshAgentPath, ['-a', authSock]);
+        core.exportVariable('SSH_AUTH_SOCK', authSock);
+        core.info('add ssh private key');
 
-        core.info('Adding private key')
+        let privateKeyFile = path.join(tmpDir, uuid());
+        fs.writeFileSync(privateKeyFile, privateKey);
+        await exec.exec(sshAddPath, ['-', privateKeyFile]);
+        core.info(`add host key of ${host}:${port} to known_hosts`)
 
-        // Add the private key
-        const key = privateKey.replace('/\r/g', '').trim() + '\n'
-        await execa('ssh-add', ['-'], {input: key})
-
-        core.info(`Adding host to known_hosts for ${host}:${port}`)
+        let knowHostInfo: string = ""
+        const options = {
+            listeners: {
+                stdout: (data: Buffer) => {
+                    knowHostInfo += data.toString() + "\n";
+                }
+            }
+        }
 
         // Add the host to the known_hosts file
-        const {stdout} = await execa('ssh-keyscan', ['-p', port.toString(), host])
-        const knownHostsFile = SSH_DIR + '/known_hosts'
-        fs.appendFileSync(knownHostsFile, stdout)
+        const out = await exec.exec(sshKeyScanPath, ['-p', port.toString(), host], options);
+        const knownHostsFile = path.join(SSH_DIR, 'known_hosts');
+        fs.appendFileSync(knownHostsFile, knowHostInfo);
         fs.chmodSync(knownHostsFile, '644')
         core.endGroup()
     } catch (error) {
